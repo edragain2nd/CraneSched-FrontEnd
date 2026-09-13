b2a68cf24b9cfbce7f0fb2c1ce15c0b1413e9833
edragain <edragain@163.com>
2026-09-13 11:45:42 +0800

    refactor cacctmgr error message

--- internal/cacctmgr/cmd.go
+++ internal/cacctmgr/cmd.go
@@ -149,18 +149,21 @@ func ParseCmdArgs(args []string) {
 	command, err := ParseCAcctMgrCommand(cmdStr)
 
 	if err != nil {
-		log.Errorf("Error: command format is incorrect %v", err)
+		log.Errorf("cacctmgr: error: command format is incorrect %v", err)
 		os.Exit(util.ErrorCmdArg)
 	}
 
 	result := executeCommand(command)
 	if result != nil {
 		var craneError *util.CraneError
-		errors.As(result, &craneError)
-		if craneError.Message != "" {
-			log.Error(craneError.Message)
+		if errors.As(result, &craneError) {
+			if craneError.Message != "" {
+				log.Errorf("cacctmgr: error: %s", strings.TrimSpace(craneError.Message))
+			}
+			os.Exit(craneError.Code)
 		}
-		os.Exit(craneError.Code)
+		log.Errorf("cacctmgr: error: %s", strings.TrimSpace(result.Error()))
+		os.Exit(util.ErrorGeneric)
 	} else {
 		os.Exit(util.ErrorSuccess)
 	}
